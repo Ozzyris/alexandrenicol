a929ed40edca56c29d707e1b83358c25b9ce370d
Alexandre Nicol <alexandre.nicol@qima.com>
2023-09-01 11:50:23 +0200

    blank boilerplate

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// =============================================================================
+// VIEWS
+// =============================================================================
+import { HomeComponent } from './views/home/home.component';
+
+
+// =============================================================================
+// ROUTES
+// =============================================================================
+const routes: Routes = [
+	{ path: 'home', component: HomeComponent, data: { title: 'Alexandre Nicol' } },
+	{ path: '',   redirectTo: 'home', pathMatch: 'full' },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
